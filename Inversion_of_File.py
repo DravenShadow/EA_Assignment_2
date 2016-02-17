f83e8663c77e754b8f24bc9a59e8f18b0fd64a60
draven <radepree@my.harrisburgu.edu>
2016-02-16 20:50:10 -0500

    Update inversion of file comments

--- Inversion_of_File.py
+++ Inversion_of_File.py
@@ -1,10 +1,31 @@
+"""
+    Author: Rowland DePree                      Inversion_of_File.py
+
+    A program designed to read in an file containing numbers on each line.  Then sort the array of numbers using
+    one of the divide and conquer sorting techniques, merger sort.  Then it will compare the original array to the newly
+    sorted array to see which spots are the same.
+"""
+
+
 def compare_file_with_array(array, sorted_array):
+    """
+    A method to compare two arrays and print out the matching positions
+    :param array:
+    :param sorted_array:
+    :return:
+    """
     print 'Here are the line numbers with array spots that match: '
     for x in range(len(array)):
         if array[x] == sorted_array[x]:
             print(x + 1)
 
+
 def read_in_list(file):
+    """
+    A method to read in a file containing numbers
+    :param file:
+    :return:
+    """
     list = []
     f = open(file, 'r')
     for line in f:
@@ -18,6 +39,12 @@ def read_in_list(file):
 
 
 def merge(arr, mid):
+    """
+    A method designed to merge two subarrays together given an array with a mid point.
+    :param arr:
+    :param mid:
+    :return:
+    """
     leftArr = arr[:mid]
     rightArr = arr[mid:]
 
@@ -43,6 +70,13 @@ def merge(arr, mid):
 
 
 def mergeSort(arr, startIndex, endIndex):
+    """
+    A recursive method designed to divided the array into two subarray's
+    :param arr:
+    :param startIndex:
+    :param endIndex:
+    :return:
+    """
     if (startIndex < endIndex):
         mid = (startIndex + endIndex) / 2
         mergeSort(arr, startIndex, mid)
@@ -51,16 +85,29 @@ def mergeSort(arr, startIndex, endIndex):
 
 
 def divideAndConquer(arr):
+    """
+    A method designed to start the merge sort algorithm
+    :param arr:
+    :return:
+    """
     length = len(arr)
     return mergeSort(arr, 0, length - 1)
 
 
 def main():
+    """
+    The main method.  Here it will take in the location of the file from the users and call on other methods to read in the file,
+    sort it, and compare it.
+    :return:
+    """
     list = read_in_list(raw_input('Enter in location of file: '))
     original_list = list[:]
     divideAndConquer(list)
     compare_file_with_array(original_list, list)
 
 
+"""
+    Starts the main method
+"""
 if __name__ == '__main__':
     main()
