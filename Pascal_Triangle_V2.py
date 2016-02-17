647ab08eb745c029be04916a10b0683578239454
draven <radepree@my.harrisburgu.edu>
2016-02-16 20:43:09 -0500

    Update pascal triangle comments

--- Pascal_Triangle_V2.py
+++ Pascal_Triangle_V2.py
@@ -1,4 +1,17 @@
+"""
+    Author: Rowland DePree                  Pascal_Triangle_V2.py
+
+    This is a program to print out pascal triangle's to a certain depth specified by the user.
+"""
+
+
 def pascal(depth, pos):
+    '''
+    A recursive method to find a particular number by adding the two previous numbers together.
+    :param depth:
+    :param pos:
+    :return:
+    '''
     if depth == pos:
         return 1
     elif depth <= -1 or pos <= -1:
@@ -8,6 +21,13 @@ def pascal(depth, pos):
 
 
 def pascal_row(row, depth, list):
+    '''
+    A recursive method find a row in pascal's triangle
+    :param row:
+    :param depth:
+    :param list:
+    :return list:
+    '''
     if row > depth:
         return
     else:
@@ -17,6 +37,12 @@ def pascal_row(row, depth, list):
 
 
 def pascal_depth(curr_depth, desired_depth):
+    '''
+    A recursive method to find the depth in pascal's triangle and print out the depth's row
+    :param curr_depth:
+    :param desired_depth:
+    :return:
+    '''
     if curr_depth > desired_depth:
         return
     else:
@@ -26,8 +52,15 @@ def pascal_depth(curr_depth, desired_depth):
 
 
 def main():
-    pascal_depth(0, 5)
+    """
+    A method to take in user specified depth
+    :return:
+    """
+    pascal_depth(0, input('Enter in desired level of pascals triangle:'))
 
 
+"""
+    Starts the main method
+"""
 if __name__ == "__main__":
     main()
